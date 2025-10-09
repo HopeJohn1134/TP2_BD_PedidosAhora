d15df4e751bc66bbd0591e558ed8c890b75687cf
HopeJohn1134 <hopejohn1134@gmail.com>
2025-10-09 12:10:12 -0300

    Update PedidosAhora.sql
    cambios en tipos de datos, eliminamos la relacion de promocion con pedido.
    reformateo de nombres de tablas,

--- PedidosAhora.sql
+++ PedidosAhora.sql
@@ -10,7 +10,7 @@ CREATE TABLE CategoriaComercio (
 
 CREATE TABLE Horario (
     id_horario INT UNSIGNED PRIMARY KEY,
-    dia_de_semana ENUM ("LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO","DOMINGO"), 
+    dia_de_semana ENUM('LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO'),
     hora_entrada TIMESTAMP,
     hora_salida TIMESTAMP
 );
@@ -37,23 +37,23 @@ CREATE TABLE HorarioXRepartidor (
 
 CREATE TABLE Comercio (
     id_comercio INT UNSIGNED PRIMARY KEY,
-    direccion VARCHAR(100) UNIQUE NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
-    telefono CHAR(15) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
     fecha_alta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id_categoria INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_categoria)
-        REFERENCES Categoria_comercio (id_categoria),
+        REFERENCES CategoriaComercio (id_categoria),
     CHECK (telefono REGEXP '^[0-9]{7,15}$')
 );
 
 CREATE TABLE Usuario (
     id_usuario INT UNSIGNED PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     domicilio VARCHAR(100) NOT NULL,
     correo_electronico VARCHAR(100) UNIQUE NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
-    telefono CHAR(15) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
     fecha_alta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CHECK (correo_electronico LIKE '%_@_%._%'),
     CHECK (telefono REGEXP '^[0-9]{7,15}$')
@@ -64,28 +64,29 @@ CREATE TABLE MedioDeTransporte (
     tipo VARCHAR(50) UNIQUE NOT NULL
 );
 
-
+-- Se podria agregar vehiculos como tabla
 CREATE TABLE Repartidor (
     id_repartidor INT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     id_transporte INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_transporte)
-        REFERENCES Medio_de_Transporte (id_transporte)
+        REFERENCES MedioDeTransporte (id_transporte)
 );
-
+-- explicar porque default ""
+-- valoracion promedio de comeercio y repartidor
 CREATE TABLE Valoracion (
     id_valoracion INT UNSIGNED PRIMARY KEY,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    comentario_comercio TEXT NOT NULL,
-    puntuacion_comercio INT UNSIGNED NOT NULL,
-    comentario_repartidor TEXT NOT NULL,
-    puntuacion_repartidor INT UNSIGNED NOT NULL,
-    CHECK (puntuacion_comercio BETWEEN 0 AND 5),
-    CHECK (puntuacion_repartidor BETWEEN 0 AND 5)
+    comentario_comercio TEXT DEFAULT '',
+    puntuacion_comercio TINYINT UNSIGNED NOT NULL,
+    comentario_repartidor TEXT DEFAULT '',
+    puntuacion_repartidor TINYINT UNSIGNED NOT NULL,
+    CHECK (puntuacion_comercio BETWEEN 1 AND 5),
+    CHECK (puntuacion_repartidor BETWEEN 1 AND 5)
 );
-
-CREATE TABLE Estado (
+-- preguntar por int vs tinyint en primary key
+CREATE TABLE EstadoDelPedido (
     id_estado INT UNSIGNED PRIMARY KEY,
     tipo VARCHAR(50) UNIQUE NOT NULL
 );
@@ -100,7 +101,7 @@ CREATE TABLE Pedido (
     id_valoracion INT UNSIGNED NOT NULL,
     id_estado INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_estado)
-        REFERENCES Estado (id_estado),
+        REFERENCES EstadoDelPedido (id_estado),
     FOREIGN KEY (id_comercio)
         REFERENCES Comercio (id_comercio),
     FOREIGN KEY (id_usuario)
@@ -113,7 +114,7 @@ CREATE TABLE Pedido (
 
 CREATE TABLE Producto (
     id_producto INT UNSIGNED PRIMARY KEY,
-    precio DECIMAL(8 , 2 ) NOT NULL,
+    precio DECIMAL(12 , 2 ) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     id_comercio INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_comercio)
@@ -124,52 +125,48 @@ CREATE TABLE MedioDePago (
     id_medio_pago INT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
+-- ver si agregar el precio pagado
 CREATE TABLE Pago (
-    pagado BOOL,
     id_pago INT UNSIGNED PRIMARY KEY,
-    id_medio_pago INT UNSIGNED NOT NULL,
+    pagado BOOL DEFAULT FALSE,
+    id_medio INT UNSIGNED NOT NULL,
     id_pedido INT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_medio_pago)
-        REFERENCES Medio_de_pago (id_medio_pago),
+    FOREIGN KEY (id_medio)
+        REFERENCES MedioDePago (id_medio_pago),
     FOREIGN KEY (id_pedido)
         REFERENCES Pedido (id_pedido)
 );
 
+-- nuestra app solo toma porcentajes para promociones de producto
+-- las promos son solo para productos y se aplican en pagos, desaparece la relacion directa de promocion y pedido
+-- estructura para almacenar los dias de la semana con bits
 CREATE TABLE Promocion (
     id_promocion INT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    valor_descuento INT UNSIGNED NOT NULL,
-    dias_de_semana DATE NOT NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_final DATE NOT NULL
+    porcentaje_descuento TINYINT UNSIGNED NOT NULL,
+    fecha_inicio DATETIME NOT NULL,
+    fecha_final DATETIME NOT NULL,
+    CHECK (porcentaje_descuento BETWEEN 0 AND 100)
 );
+
 -- preguntar al profe ==================================================================
 -- n:n y 1:n
-CREATE TABLE TieneEsta (
+CREATE TABLE ProductoXPedido (
+    id_productoxpedido INT UNSIGNED PRIMARY KEY,
     id_producto INT UNSIGNED NOT NULL,
     id_pedido INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id_pedido , id_producto),
     FOREIGN KEY (id_producto)
         REFERENCES Producto (id_producto),
     FOREIGN KEY (id_pedido)
         REFERENCES Pedido (id_pedido)
 );
 
-CREATE TABLE EstaDentroTiene (
+CREATE TABLE PromocionXProducto (
+    id_promocionxproducto INT UNSIGNED PRIMARY KEY,
     id_promocion INT UNSIGNED NOT NULL,
     id_producto INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_promocion)
         REFERENCES Promocion (id_promocion),
     FOREIGN KEY (id_producto)
         REFERENCES Producto (id_producto)
-);
-
-CREATE TABLE AsignadoTiene (
-    id_promocion INT UNSIGNED NOT NULL,
-    id_pedido INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id_pedido , id_promocion),
-    FOREIGN KEY (id_promocion)
-        REFERENCES Promocion (id_promocion),
-    FOREIGN KEY (id_pedido)
-        REFERENCES Pedido (id_pedido)
 );
