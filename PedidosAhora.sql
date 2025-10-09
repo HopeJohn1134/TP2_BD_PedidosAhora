dce4d3e5fcab7380e45045abb6b758a3302eb5ed
HopeJohn1134 <hopejohn1134@gmail.com>
2025-10-09 14:07:19 -0300

    añadido auto_increment

--- PedidosAhora.sql
+++ PedidosAhora.sql
@@ -2,21 +2,21 @@ CREATE DATABASE PedidosAhora;
 USE PedidosAhora;
 
 CREATE TABLE CategoriaComercio (
-    id_categoria INT UNSIGNED PRIMARY KEY,
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
     categoria VARCHAR(20) UNIQUE NOT NULL
 );
 -- enum los dias de semana
 
 
 CREATE TABLE Horario (
-    id_horario INT UNSIGNED PRIMARY KEY,
+    id_horario INT AUTO_INCREMENT PRIMARY KEY,
     dia_de_semana ENUM('LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO'),
     hora_entrada TIMESTAMP,
     hora_salida TIMESTAMP
 );
 -- preguntarle al profe ======================================================================
 CREATE TABLE HorarioXComercio (
-    id_horarioxcomercio INT UNSIGNED PRIMARY KEY,
+    id_horarioxcomercio INT AUTO_INCREMENT PRIMARY KEY,
     id_horario INT UNSIGNED NOT NULL,
     id_comercio INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_horario)
@@ -26,7 +26,7 @@ CREATE TABLE HorarioXComercio (
 );
 
 CREATE TABLE HorarioXRepartidor (
-    id_horarioxrepartidor INT UNSIGNED PRIMARY KEY,
+    id_horarioxrepartidor INT AUTO_INCREMENT PRIMARY KEY,
     id_horario INT UNSIGNED NOT NULL,
     id_repartidor INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_horario)
@@ -36,7 +36,7 @@ CREATE TABLE HorarioXRepartidor (
 );
 
 CREATE TABLE Comercio (
-    id_comercio INT UNSIGNED PRIMARY KEY,
+    id_comercio INT AUTO_INCREMENT PRIMARY KEY,
     direccion VARCHAR(100) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     telefono VARCHAR(15) NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE Comercio (
 );
 
 CREATE TABLE Usuario (
-    id_usuario INT UNSIGNED PRIMARY KEY,
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     domicilio VARCHAR(100) NOT NULL,
@@ -60,13 +60,13 @@ CREATE TABLE Usuario (
 );
 
 CREATE TABLE MedioDeTransporte (
-    id_transporte INT UNSIGNED PRIMARY KEY,
+    id_transporte INT AUTO_INCREMENT PRIMARY KEY,
     tipo VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Se podria agregar vehiculos como tabla
 CREATE TABLE Repartidor (
-    id_repartidor INT UNSIGNED PRIMARY KEY,
+    id_repartidor INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     id_transporte INT UNSIGNED NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE Repartidor (
 -- explicar porque default ""
 -- valoracion promedio de comeercio y repartidor
 CREATE TABLE Valoracion (
-    id_valoracion INT UNSIGNED PRIMARY KEY,
+    id_valoracion INT AUTO_INCREMENT PRIMARY KEY,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     comentario_comercio TEXT DEFAULT '',
     puntuacion_comercio TINYINT UNSIGNED NOT NULL,
@@ -87,13 +87,13 @@ CREATE TABLE Valoracion (
 );
 -- preguntar por int vs tinyint en primary key
 CREATE TABLE EstadoDelPedido (
-    id_estado INT UNSIGNED PRIMARY KEY,
+    id_estado INT AUTO_INCREMENT PRIMARY KEY,
     tipo VARCHAR(50) UNIQUE NOT NULL
 );
 
 
 CREATE TABLE Pedido (
-    id_pedido INT UNSIGNED PRIMARY KEY,
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     domicilio VARCHAR(50) NOT NULL,
     id_comercio INT UNSIGNED NOT NULL,
     id_usuario INT UNSIGNED NOT NULL,
@@ -113,7 +113,7 @@ CREATE TABLE Pedido (
 );
 
 CREATE TABLE Producto (
-    id_producto INT UNSIGNED PRIMARY KEY,
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
     precio DECIMAL(12 , 2 ) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     id_comercio INT UNSIGNED NOT NULL,
@@ -122,12 +122,12 @@ CREATE TABLE Producto (
 );
 
 CREATE TABLE MedioDePago (
-    id_medio_pago INT UNSIGNED PRIMARY KEY,
+    id_medio_pago INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
 -- ver si agregar el precio pagado
 CREATE TABLE Pago (
-    id_pago INT UNSIGNED PRIMARY KEY,
+    id_pago INT AUTO_INCREMENT PRIMARY KEY,
     pagado BOOL DEFAULT FALSE,
     id_medio INT UNSIGNED NOT NULL,
     id_pedido INT UNSIGNED NOT NULL,
@@ -141,7 +141,7 @@ CREATE TABLE Pago (
 -- las promos son solo para productos y se aplican en pagos, desaparece la relacion directa de promocion y pedido
 -- estructura para almacenar los dias de la semana con bits
 CREATE TABLE Promocion (
-    id_promocion INT UNSIGNED PRIMARY KEY,
+    id_promocion INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     porcentaje_descuento TINYINT UNSIGNED NOT NULL,
     fecha_inicio DATETIME NOT NULL,
@@ -152,7 +152,7 @@ CREATE TABLE Promocion (
 -- preguntar al profe ==================================================================
 -- n:n y 1:n
 CREATE TABLE ProductoXPedido (
-    id_productoxpedido INT UNSIGNED PRIMARY KEY,
+    id_productoxpedido INT AUTO_INCREMENT PRIMARY KEY,
     id_producto INT UNSIGNED NOT NULL,
     id_pedido INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_producto)
@@ -162,7 +162,7 @@ CREATE TABLE ProductoXPedido (
 );
 
 CREATE TABLE PromocionXProducto (
-    id_promocionxproducto INT UNSIGNED PRIMARY KEY,
+    id_promocionxproducto INT AUTO_INCREMENT PRIMARY KEY,
     id_promocion INT UNSIGNED NOT NULL,
     id_producto INT UNSIGNED NOT NULL,
     FOREIGN KEY (id_promocion)
